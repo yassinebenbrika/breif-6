a5a75fcf3678ed3cc3e6f485bd7822c18f988a0f
yassinebenbrika <freudsigmund437@gmail.com>
2023-11-27 22:46:54 +0100

    update

--- landing_po.php
+++ landing_po.php
@@ -1,15 +1,13 @@
 <?php
 require_once('config/db.php');
 require_once('config/addproject.php');
-$result = mysqli_query($con, "SELECT * FROM projects");
-$result_1 = mysqli_query($con, "SELECT * FROM users , teams where teams.id = users.teamId and role ='member' ");
-$result_3 = mysqli_query($con, "SELECT * FROM teams , projects where teams.projectId = projects.id  ");
-$result_2 = mysqli_query($con, "SELECT *
+$result = mysqli_query($con, "SELECT * FROM projects , equipe where equipe.id = projects.teamId");
+$result_1 = mysqli_query($con, "SELECT * FROM users , equipe where equipe.id = users.teamId and role ='member' ");
+$result_3 = mysqli_query($con, "SELECT * FROM equipe , projects where equipe.id = projects.teamId  ");
+$result_2 = mysqli_query($con, "SELECT DISTINCT users.*, equipe.*
     FROM users
-    JOIN teams ON teams.id = users.teamId
-    JOIN projects ON teams.id = projects.teamId
+    JOIN equipe ON equipe.id = users.teamId
     WHERE users.role = 'scrum master'");
- 
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -58,7 +56,7 @@ $result_2 = mysqli_query($con, "SELECT *
         </nav>
     </header>
     <div class="flex">
-        <aside id="sidebar" class=" lg:flex flex-col w-64 h-screen px-4 py-8 overflow-y-auto bg-white border-r rtl:border-r-0 rtl:border-l dark:bg-gray-900 dark:border-gray-700 z-1000	">
+        <aside id="sidebar" class=" lg:flex flex-col w-64 h-screen px-4 py-8 overflow-y-auto bg-white border-r rtl:border-r-0 rtl:border-l dark:bg-gray-900 dark:border-gray-700 h-screeen	">
             <div class="flex flex-col justify-between flex-1 mt-6">
                 <nav>
                     <a id="dashboardButton" class="flex items-center px-4 py-2 mt-5 text-gray-600 transition-colors duration-300 transform rounded-md dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700" href="#">
@@ -101,7 +99,7 @@ $result_2 = mysqli_query($con, "SELECT *
 
                         <span class="mx-4 font-medium">Teams</span>
                     </a>
-                    <a id="logInButton" class="flex items-center px-4 py-2 mt-5 text-gray-600 transition-colors duration-300 transform rounded-md dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700" href="#">
+                    <a href="login.php"id="logInButton" class="flex items-center px-4 py-2 mt-5 text-gray-600 transition-colors duration-300 transform rounded-md dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700" href="#">
                         <svg class="w-5 h-5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M10.3246 4.31731C10.751 2.5609 13.249 2.5609 13.6754 4.31731C13.9508 5.45193 15.2507 5.99038 16.2478 5.38285C17.7913 4.44239 19.5576 6.2087 18.6172 7.75218C18.0096 8.74925 18.5481 10.0492 19.6827 10.3246C21.4391 10.751 21.4391 13.249 19.6827 13.6754C18.5481 13.9508 18.0096 15.2507 18.6172 16.2478C19.5576 17.7913 17.7913 19.5576 16.2478 18.6172C15.2507 18.0096 13.9508 18.5481 13.6754 19.6827C13.249 21.4391 10.751 21.4391 10.3246 19.6827C10.0492 18.5481 8.74926 18.0096 7.75219 18.6172C6.2087 19.5576 4.44239 17.7913 5.38285 16.2478C5.99038 15.2507 5.45193 13.9508 4.31731 13.6754C2.5609 13.249 2.5609 10.751 4.31731 10.3246C5.45193 10.0492 5.99037 8.74926 5.38285 7.75218C4.44239 6.2087 6.2087 4.44239 7.75219 5.38285C8.74926 5.99037 10.0492 5.45193 10.3246 4.31731Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                             <path d="M15 12C15 13.6569 13.6569 15 12 15C10.3431 15 9 13.6569 9 12C9 10.3431 10.3431 9 12 9C13.6569 9 15 10.3431 15 12Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
@@ -129,6 +127,9 @@ $result_2 = mysqli_query($con, "SELECT *
                                 Deadline
                             </th>
                             <th scope="col" class="px-6 py-3 text-center">
+                                team
+                            </th>
+                            <th scope="col" class="px-6 py-3 text-center">
                                 action
                             </th>
                         </tr>
@@ -141,7 +142,7 @@ $result_2 = mysqli_query($con, "SELECT *
                                 <td class="text-center"><?php echo $row['project_name'] ?></td>
                                 <td class="text-center"><?php echo $row['description'] ?></td>
                                 <td class="text-center"><?php echo $row['deadline'] ?></td>
-                                <td class="text-center"><?php echo $row['teamId'] ?></td>
+                                <td class="text-center"><?php echo $row['team'] ?></td>
 
 
                                 <td class="px-4 py-4 text-sm ">
@@ -242,9 +243,6 @@ $result_2 = mysqli_query($con, "SELECT *
                                 email
                             </th>
                             <th scope="col" class="px-6 py-3 text-center">
-                                project
-                            </th>
-                            <th scope="col" class="px-6 py-3 text-center">
                                 action
                             </th>
 
@@ -260,8 +258,6 @@ $result_2 = mysqli_query($con, "SELECT *
                                 <td class="text-center"><?php echo $row['phone'] ?></td>
                                 <td class="text-center"><?php echo $row['team'] ?></td>
                                 <td class="text-center"><?php echo $row['email'] ?></td>
-
-                                <td class="text-center"><?php echo $row['project_name'] ?></td>
                                 <td class="px-4 py-4 text-sm">
                                     <div class="flex items-center justify-center gap-x-6">
                                         <button class="text-gray-500 transition-colors duration-200 dark:hover:text-yellow-500 dark:text-gray-300 hover:text-yellow-500 focus:outline-none flex items-center">
